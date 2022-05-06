c4ba0dce4c87960820a8c4bd2a6bbfca57bd6d7f
CarolaPessotto <885384@stud.unive.it>
2022-05-06 12:18:21 +0200

    Implementazione custom errori 404 e 403. NB html da rifinire

--- migrations/versions/e9d14da1e18d_.py
+++ migrations/versions/e9d14da1e18d_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 29ceefacefc5
+Revision ID: e9d14da1e18d
 Revises: 
-Create Date: 2022-05-05 10:03:32.970392
+Create Date: 2022-05-06 11:52:20.299142
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '29ceefacefc5'
+revision = 'e9d14da1e18d'
 down_revision = None
 branch_labels = None
 depends_on = None
