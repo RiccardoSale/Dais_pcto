5e8cec1847829fdca2fafebdc502ebd7e730a78d
SerenaZanon <887050@stud.unive.it>
2022-06-27 00:30:15 +0200

    inizio commenti

--- dais_pcto/Auth/forms.py
+++ dais_pcto/Auth/forms.py
@@ -3,27 +3,23 @@ from wtforms import StringField, SelectField, IntegerField, DateField, PasswordF
 from wtforms.validators import InputRequired, Length, Regexp, Email, ValidationError, Optional
 from .models import user_with_email
 
-
+# Per il login viene richiesta una email e una password
 class LoginForm(FlaskForm):
     email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
     password = PasswordField(validators=[Optional()])
 
-
+# Per iscriversi è necessario inserire una email, una password, un nome e un cognome
 class SignupForm(FlaskForm):
     email = StringField(validators=[InputRequired(), Email(), Length(1, 64)])
     password = PasswordField(validators=[InputRequired(), Length(8, 72)])
+    # Il nome e il cognome devono contenere caratteri consoni
     name = StringField(validators=[InputRequired(), Length(3, 64, message="Please provide a valid name"), Regexp(
-        "^[A-Za-z][A-Za-z0-9_.]*$",  # trovare regex per lettere italiane e straniere
-        0,
-        "Name can have only letters",
-    )])
+        "^[A-Za-z][A-Za-z_. ]*$", 0, "Name can have only letters", )])
     surname = StringField(
         validators=[InputRequired(), Length(3, 20, message="Please provide a valid name"), Regexp(
-            "^[A-Za-z][A-Za-z0-9_.]*$",
-            0,
-            "Surname can have only letters",
-        )])
+            "^[A-Za-z][A-Za-z_. ]*$", 0, "Surname can have only letters", )])
 
+    # Controllo sulla email (la email inserita nell'iscrizione non deve già esistere)
     def validate_email(self, field):
         if user_with_email(field.data).first():
             raise ValidationError("Email già registrata!")
--- dais_pcto/Auth/models.py
+++ dais_pcto/Auth/models.py
@@ -2,45 +2,42 @@ from flask_login import UserMixin
 from dais_pcto.app import db
 from dais_pcto.module_extensions import bcrypt
 
+# Creazione della tabella nata dalla relazione tra utente e corsi
 user_course = db.Table('user_course',
                        db.Column('user_id', db.Integer, db.ForeignKey('users._user_id')),
                        db.Column('course_id', db.Integer, db.ForeignKey('courses._course_id'))
                        )
 
+# Creazione della tabella nata dalla relazione tra utente e lezioni
 user_lesson = db.Table('user_lesson',
                        db.Column('user_id', db.Integer, db.ForeignKey('users._user_id')),
                        db.Column('lesson_id', db.Integer, db.ForeignKey('lessons._lesson_id'))
                        )
 
-
-# tabella secondaria per relazione molti a molti tra utenti e corsi
-
-
 class User(UserMixin, db.Model):
     __tablename__ = "users"
-
     _user_id = db.Column(db.Integer, primary_key=True)
     _name = db.Column(db.String(64), unique=False, nullable=False)
     _surname = db.Column(db.String(64), unique=False, nullable=False)
     _email = db.Column(db.String(60), unique=True, nullable=False, index=True)
-    _password = db.Column(db.String(128),
-                          nullable=False)  # lunghezza dopo hashing CONTROLLARE DOPO AVER LIMITATO PASSWORD A TOT CARATTERI
+    _password = db.Column(db.String(128), nullable=False)  # lunghezza dopo hashing CONTROLLARE DOPO AVER LIMITATO PASSWORD A TOT CARATTERI
     # cambiare form iscrizione aggiungendo campo choice -> con scuole !!!
+    # Campo per l'identificazione del ruolo dell'utente
     _role = db.Column(db.String(10), nullable=False,
-                      default="user")  # serve per l identificazione user /admin / professor
-
+                      default="user")
+    # Campo nato dalla relazione molti a molti con la tabella 'courses'
     _courses = db.relationship("Course", secondary=user_course,
-                               back_populates="_users")  # necessaria per relazione molti a molti !
-
+                               back_populates="_users")
+    # Campo nato dalla relazione molti a molti con la tabella 'lessons'
     _lessons = db.relationship("Lesson", secondary=user_lesson,
                                back_populates="_users")
-
+    # Campo nato dalla relazioni uno a molti con la tabella 'h_schools'
     _school = db.Column(db.String, db.ForeignKey(
         'h_schools._hschool_code'))
-
-    # uno a molti -> legata a chiave esterna professor
+    # Campo nato dalla relazione uno a molti con la tabella 'courses' (solo per gli utenti con ruolo '_professor')
     _courses_prof = db.relationship('Course', backref='users', cascade="all, delete")
 
+    # Inizializzazione
     def __init__(self, name, surname, email, password, role):
         self._name = name
         self._surname = surname
@@ -48,54 +45,67 @@ class User(UserMixin, db.Model):
         self._password = password
         self._role = role
 
+
     def __repr__(self):
         return self._email
 
+    # Aggiunta di un corso
     def subscribe_course(self, course):
         if course:
             self._courses.append(course)
             self.update()
 
+    # Rimozione di un corso
     def unsubscribe_course(self, course):
         if course:
             self._courses.remove(course)
             self.update()
 
+    # Aggiunta di una lezione
     def subscribe_lesson(self, lesson):
         if lesson:
             self._lessons.append(lesson)
             self.update()
 
+    # Aggiunta di un corso per gli utenti con ruolo '_professor'
     def professor_course(self, course):
         if course:
             self._courses_prof.append(course)
             self.update()
 
+    # Settaggio della password e criptazione
     def set_password(self, password):
         if password != "":
             self._password = bcrypt.generate_password_hash(password).decode('utf8')
 
+    # Settaggio del nome
     def set_name(self, name):
         if name != "":
             self._name = name
 
+    # Settaggio del cognome
     def set_surname(self, surname):
         if surname != "":
             self._surname = surname
 
+    # Settaggio della email
     def set_email(self, email):
         if email != "":
             self._email = email
 
+    # Ritorno dell'id dell'utente
     def get_id(self):
         return self._user_id
 
+# Ritorno uìdell'utente con una determinata email
 def user_with_email(email):
     return db.session.query(User).filter_by(_email=email)
 
+# Ritorno di un utente con un determinato id
 def user_with_id(id):
     return db.session.query(User).filter_by(_user_id=id)
 
+# Ritorno di un utente con un determinato ruolo
 def users_with_role(role):
     return db.session.query(User).filter_by(_role=role)
 
