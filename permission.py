c3b50f20a198dad951f7924ddcd58329cb4831f5
SerenaZanon <79572052+SerenaZanon@users.noreply.github.com>
2022-07-03 18:01:17 +0200

    update

--- permission.py
+++ permission.py
@@ -13,15 +13,15 @@ with app.app_context():
             db.session.execute('GRANT DELETE, INSERT, UPDATE, SELECT ON TABLE users TO admin;')
             db.session.execute('GRANT SELECT, INSERT ,UPDATE  ON TABLE users TO segreteria;')
             db.session.execute('GRANT SELECT ON TABLE users TO professor;')
-            # # PERMESSI TABELLA Courses
+            # PERMESSI TABELLA Courses
             db.session.execute('GRANT DELETE, SELECT, INSERT, UPDATE ON TABLE courses TO admin;')
             db.session.execute('GRANT DELETE, SELECT, INSERT, UPDATE ON TABLE courses TO segreteria;')
             db.session.execute('GRANT DELETE, SELECT, INSERT, UPDATE ON TABLE courses TO professor;')
-            # # PERMESSI TABELLA Schools
+            # PERMESSI TABELLA Schools
             db.session.execute('GRANT DELETE, SELECT, INSERT, UPDATE ON TABLE h_schools TO admin;')
             db.session.execute('GRANT DELETE, SELECT, INSERT, UPDATE ON TABLE h_schools TO segreteria;')
             db.session.execute('GRANT  SELECT ON TABLE h_schools TO professor;')
-            # # PERMESSI TABELLA LESSONS
+            # PERMESSI TABELLA Lessons
             db.session.execute('GRANT DELETE, SELECT, INSERT, UPDATE ON TABLE lessons TO admin;')
             db.session.execute('GRANT SELECT, INSERT, UPDATE , DELETE ON TABLE lessons TO segreteria;')
             db.session.execute('GRANT SELECT, INSERT, UPDATE , DELETE ON TABLE lessons TO professor;')
