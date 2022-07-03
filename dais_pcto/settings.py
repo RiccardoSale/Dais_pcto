039cdc8dab2a11a97f68af00aeaf046829f9922e
SerenaZanon <79572052+SerenaZanon@users.noreply.github.com>
2022-07-03 18:21:58 +0200

    update

--- dais_pcto/settings.py
+++ dais_pcto/settings.py
@@ -3,17 +3,16 @@
 import os
 from datetime import timedelta
 
-
+# Configurazione base
 class Config(object):
     """Base configuration."""
 
     SECRET_KEY = "put_secret_key_here"
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
-    #CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
+# Configurazione di produzione
 class ProdConfig(Config):
     """Production configuration."""
 
@@ -22,16 +21,10 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
                                              "postgresql://postgres:postgres@localhost:5432/dais_pcto")
 
-
+# Configurazione di sviluppo
 class DevConfig(Config):
     """Development configuration."""
 
-    #ENV = 'dev'
     DEBUG = True
-    #DB_NAME = 'dev.db'
-    # Put the db file in project root
-    #DB_PATH = os.path.join(Config.PROJECT_ROOT, DB_NAME)
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/dais_pcto'
-    #CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
-
 
