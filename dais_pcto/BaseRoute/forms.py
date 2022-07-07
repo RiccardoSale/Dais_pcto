0058f065a1e9304510a1785170e4fb7b4d8db0a7
SerenaZanon <79572052+SerenaZanon@users.noreply.github.com>
2022-07-07 11:06:59 +0200

    update

--- dais_pcto/BaseRoute/forms.py
+++ dais_pcto/BaseRoute/forms.py
@@ -14,20 +14,20 @@ class EditProfile(FlaskForm):
         validators=[InputRequired(message="La password che si vuol modificare è necessaria per il cambio credenziali"), Length(8, 72)])
     # Il nome e il cognome devono contenere caratteri consoni (non possono esserci numeri o caratteri particolari)
     name = StringField(validators=[Length(3, 64, message="Inserire un nome valido"), Optional(), Regexp(
-        "^[A-Za-z][A-Za-z_. ]*$", 0, "Il nome deve contenere caratteri consoni",)])
+        "^[A-Za-z][A-Za-z_.àèìòù, ]*$", 0, "Il nome deve contenere caratteri consoni",)])
     surname = StringField(
         validators=[Length(3, 20, message="Inserire un cognome valido"), Optional(), Regexp(
-            "^[A-Za-z][A-Za-z_. ]*$", 0, "Il cognome deve contenere caratteri consoni",)])
+            "^[A-Za-z][A-Za-z_.àèìòù, ]*$", 0, "Il cognome deve contenere caratteri consoni",)])
 
 # I dati richiesti per associare un utente alla scuola di provenienza sono
 # il nome, il cognome, l'id dell'utente e la scuola
 class EditProfileSeg(FlaskForm):
     # Il nome e il cognome devono contenere caratteri consoni (non possono esserci numeri o caratteri particolari)
     name = StringField(validators=[Length(3, 64, message="Inserire un nome valido"), Optional(), Regexp(
-        "^[A-Za-z][A-Za-z_. ]*$", 0, "Il nome deve contenere caratteri consoni",)])
+        "^[A-Za-z][A-Za-z_.àèìòù, ]*$", 0, "Il nome deve contenere caratteri consoni",)])
     surname = StringField(
         validators=[Length(3, 20, message="Inserire un cognome valido"), Optional(), Regexp(
-            "^[A-Za-z][A-Za-z_. ]*$", 0, "Il cognome deve contenere caratteri consoni", )])
+            "^[A-Za-z][A-Za-z_.àèìòù, ]*$", 0, "Il cognome deve contenere caratteri consoni", )])
     school = SelectField(validators=[Optional()], choices=all_schools)
     user = HiddenField(validators=[Optional()])
 
