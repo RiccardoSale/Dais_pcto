87dc4dbee27da9d9a0e807fb1029c2ba78086722
SerenaZanon <79572052+SerenaZanon@users.noreply.github.com>
2022-07-07 11:06:09 +0200

    update

--- dais_pcto/Auth/forms.py
+++ dais_pcto/Auth/forms.py
@@ -14,10 +14,10 @@ class SignupForm(FlaskForm):
     password = PasswordField(validators=[InputRequired(), Length(8, 72)])
     # Il nome e il cognome devono contenere caratteri consoni (non possono esserci numeri o caratteri particolari)
     name = StringField(validators=[InputRequired(), Length(3, 64, message="Inserire un nome valido"), Regexp(
-        "^[A-Za-z][A-Za-z_. ]*$", 0, "Il nome deve contenere caratteri consoni", )])
+        "^[A-Za-z][A-Za-z_.àèìòù, ]*$", 0, "Il nome deve contenere caratteri consoni", )])
     surname = StringField(
         validators=[InputRequired(), Length(3, 20, message="Inserire un cognome valido"), Regexp(
-            "^[A-Za-z][A-Za-z_. ]*$", 0, "Il cognome deve contenere caratteri consoni", )])
+            "^[A-Za-z][A-Za-z_.àèìòù, ]*$", 0, "Il cognome deve contenere caratteri consoni", )])
 
     # Controllo sulla email (la email inserita nell'iscrizione non deve già esistere)
     def validate_email(self, field):
